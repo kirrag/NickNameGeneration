20349ccd1c37d0793a1f07df084f7ae294463501
Garrik Ten <ten@otux.ru>
2023-04-24 03:17:43 +0300

    formated code

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,47 +5,49 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Arrays;
 
 public class Main {
-	
+
 	public static AtomicInteger beauty3 = new AtomicInteger(0);
 	public static AtomicInteger beauty4 = new AtomicInteger(0);
 	public static AtomicInteger beauty5 = new AtomicInteger(0);
 
-
 	public static String generateText(String letters, int length) {
-        Random random = new Random();
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return text.toString();
-    }
+		Random random = new Random();
+		StringBuilder text = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			text.append(letters.charAt(random.nextInt(letters.length())));
+		}
+		return text.toString();
+	}
 
 	public static void main(String[] args) {
 
 		Random random = new Random();
-        String[] texts = new String[100_000];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abc", 3 + random.nextInt(3));
-        }
+		String[] texts = new String[100_000];
+		for (int i = 0; i < texts.length; i++) {
+			texts[i] = generateText("abc", 3 + random.nextInt(3));
+		}
 
 		// слово является палиндромом
 		new Thread(() -> {
-			for (String text: texts) {
+			for (String text : texts) {
 				StringBuilder reverse = new StringBuilder();
 				for (int i = text.length() - 1; i >= 0; i--) {
 					reverse.append(text.charAt(i));
 				}
 				if (text.equals(reverse.toString())) {
-					if(text.length() == 3) beauty3.incrementAndGet();
-					if(text.length() == 4) beauty4.incrementAndGet();
-					if(text.length() == 5) beauty5.incrementAndGet();
+					if (text.length() == 3)
+						beauty3.incrementAndGet();
+					if (text.length() == 4)
+						beauty4.incrementAndGet();
+					if (text.length() == 5)
+						beauty5.incrementAndGet();
 				}
-			}	
+			}
 		}).start();
 
 		// слово состоит из одной и той же буквы,
 		new Thread(() -> {
-			for (String text: texts) {
+			for (String text : texts) {
 				boolean isEquals = true;
 				for (int i = 0; i < 0; i++) {
 					if (text.charAt(i) != text.charAt(0)) {
@@ -53,28 +55,34 @@ public class Main {
 					}
 				}
 				if (isEquals) {
-					if(text.length() == 3) beauty3.incrementAndGet();
-					if(text.length() == 4) beauty4.incrementAndGet();
-					if(text.length() == 5) beauty5.incrementAndGet();
+					if (text.length() == 3)
+						beauty3.incrementAndGet();
+					if (text.length() == 4)
+						beauty4.incrementAndGet();
+					if (text.length() == 5)
+						beauty5.incrementAndGet();
 				}
 			}
 		}).start();
 
 		// буквы в слове идут по возрастанию
 		new Thread(() -> {
-			for (String text: texts) {
+			for (String text : texts) {
 				boolean isSorted = false;
 				char[] textChars = text.toCharArray();
 				Arrays.sort(textChars);
 				String textSorted = new String(textChars);
-				if(text.equals(textSorted)) {
+				if (text.equals(textSorted)) {
 					isSorted = true;
 				}
 
 				if (isSorted) {
-					if(text.length() == 3) beauty3.incrementAndGet();
-					if(text.length() == 4) beauty4.incrementAndGet();
-					if(text.length() == 5) beauty5.incrementAndGet();
+					if (text.length() == 3)
+						beauty3.incrementAndGet();
+					if (text.length() == 4)
+						beauty4.incrementAndGet();
+					if (text.length() == 5)
+						beauty5.incrementAndGet();
 				}
 			}
 		}).start();
